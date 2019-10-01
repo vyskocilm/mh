df5eef832fc34d6a943b8028a03c7efe7d72f5c0
Michal Vyskocil <michal.vyskocil@gmail.com>
2019-10-01 11:34:38 +0200

    Problem: estore tests fails
    Solution: add "group1" to all the code making it compatible with
    group-ready code

--- cmd/estore_test.go
+++ cmd/estore_test.go
@@ -13,34 +13,35 @@ func TestEStore(t *testing.T) {
 	assert.NoError(err)
 	assert.Equal(0, len(estore.List()))
 
-	estore.Add("192.168.1.42", "srvc1.test")
+	estore.Add("192.168.1.42", "srvc1.test", "group1")
 	assert.Equal(1, len(estore.List()))
 
 	// add duplicate - same IP
-	estore.Add("192.168.1.42", "srvc42.test")
-	assert.Equal(2, len(estore.List()))
+	estore.Add("192.168.1.42", "srvc42.test", "group1")
+    t.Logf("BAF1: estore.List()=%+v", estore.List())
+	assert.Equal(2, len(estore.List()["group1"]))
 
 	// add duplicate - same Name
-	estore.Add("192.168.1.142", "srvc1.test")
-	assert.Equal(3, len(estore.List()))
+	estore.Add("192.168.1.142", "srvc1.test", "group1")
+	assert.Equal(3, len(estore.List()["group1"]))
 
-	estore.Add("192.168.1.142", "srvc142.test")
-	assert.Equal(4, len(estore.List()))
+	estore.Add("192.168.1.142", "srvc142.test", "group1")
+	assert.Equal(4, len(estore.List()["group1"]))
 
 	// drop non existing item
-	err = estore.Del("not.there.com")
+	err = estore.Del("not.there.com", "group1")
 	assert.Equal(errIPOrNameNotFound, err)
-	assert.Equal(4, len(estore.List()))
+	assert.Equal(4, len(estore.List()["group1"]))
 
 	// delete by Name
-	err = estore.Del("srvc142.test")
+	err = estore.Del("srvc142.test", "group1")
 	assert.NoError(err)
-	assert.Equal(3, len(estore.List()))
+	assert.Equal(3, len(estore.List()["group1"]))
 
 	// delete by IP
-	err = estore.Del("192.168.1.42")
+	err = estore.Del("192.168.1.42", "group1")
 	assert.NoError(err)
-	assert.Equal(2, len(estore.List()))
+	assert.Equal(2, len(estore.List()["group1"]))
 
 	err = estore.Commit()
 	assert.NoError(err)
@@ -63,34 +64,34 @@ func TestEStoreMx(t *testing.T) {
 	assert.NoError(err)
 	assert.Equal(0, len(estore.List()))
 
-	estore.Add("192.168.1.42", "srvc1.test")
+	estore.Add("192.168.1.42", "srvc1.test", "group1")
 	assert.Equal(1, len(estore.List()))
 
 	// add duplicate - same IP
-	estore.Add("192.168.1.42", "srvc42.test")
-	assert.Equal(2, len(estore.List()))
+	estore.Add("192.168.1.42", "srvc42.test", "group1")
+	assert.Equal(2, len(estore.List()["group1"]))
 
 	// add duplicate - same Name
-	estore.Add("192.168.1.142", "srvc1.test")
-	assert.Equal(3, len(estore.List()))
+	estore.Add("192.168.1.142", "srvc1.test", "group1")
+	assert.Equal(3, len(estore.List()["group1"]))
 
-	estore.Add("192.168.1.142", "srvc142.test")
-	assert.Equal(4, len(estore.List()))
+	estore.Add("192.168.1.142", "srvc142.test", "group1")
+	assert.Equal(4, len(estore.List()["group1"]))
 
 	// drop non existing item
-	err = estore.Del("not.there.com")
+	err = estore.Del("not.there.com", "group1")
 	assert.Equal(errIPOrNameNotFound, err)
-	assert.Equal(4, len(estore.List()))
+	assert.Equal(4, len(estore.List()["group1"]))
 
 	// delete by Name
-	err = estore.Del("srvc142.test")
+	err = estore.Del("srvc142.test", "group1")
 	assert.NoError(err)
-	assert.Equal(3, len(estore.List()))
+	assert.Equal(3, len(estore.List()["group1"]))
 
 	// delete by IP
-	err = estore.Del("192.168.1.42")
+	err = estore.Del("192.168.1.42", "group1")
 	assert.NoError(err)
-	assert.Equal(2, len(estore.List()))
+	assert.Equal(2, len(estore.List()["group1"]))
 
 	err = estore.Close()
 	assert.NoError(err)
